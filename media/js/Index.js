d916f574590b496bb743bd88d1a9ba5ce6ea6669
kcs2981 <kcs2981>
2016-06-17 22:25:01 +0800

    readme

--- media/js/Index.js
+++ media/js/Index.js
@@ -1,3 +1,18 @@
 $(function(){
-	
+    var indexs = 1;
+    function autoScroll(){
+        if(indexs ==2){
+            $(".indexScrollList").eq(0).removeAttr("style").clone(true).appendTo($(".scroll_mod"));
+            $(".indexScrollList").eq(0).css({"margin-left":-1300*(indexs-1)}).animate({"margin-left":-1300*indexs},300,function(){
+                $(".indexScrollList").eq(0).remove();
+            });
+            console.log(indexs)
+            indexs = 1;
+        }else{
+            $(".indexScrollList").eq(0).animate({"margin-left":-1300*indexs});
+            indexs++;
+            console.log(indexs)
+        }
+    }
+    setInterval(autoScroll,3000)
 });
